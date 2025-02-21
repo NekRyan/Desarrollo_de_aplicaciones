24f98d751ff2f976bf9b4f7878c63b8b00201115
NekRyan <nekryanjm@gmail.com>
2025-02-20 22:30:18 -0500

    errores corregidos correctamente 2

--- Ses2.py
+++ Ses2.py
@@ -4,17 +4,17 @@ from algoritmos import AlgoritmosSecuenciales
 st.title("Algoritmos secuenciales con POO Streamlit")
 
 #entrada de usuario
-numero = st.number_input("ingrese un numero entero positivo", min_value=1, value=5, step=1)
+numero = st.number_input("Ingrese un numero entero positivo", min_value=1, value=5, step=1)
 
 #Instanciamos la clase con el numero ingresado
 algoritmos = AlgoritmosSecuenciales(numero)
 
 #Crear botones para ejecutar los algoritmos secunciales
-if st.button("calcular suma de N numeros"):
-    st.success(f"la suma de los primeros {numero} numero es: {algoritmos.suma_n_numeros()}")
+if st.button("Calcular suma de N numeros"):
+    st.success(f"La suma de los primeros {numero} numero es: {algoritmos.suma_n_numeros()}")
 
-if st.button("calcular factorial"):
+if st.button("Calcular factorial"):
     st.success(f"el factorial de {numero} es: {algoritmos.factorial()}")
 
-if st.button("generar secuencia de fibonacci"):
-    st.success(f"la secuencia de fibonacci con {numero} terminos es: {algoritmos.fibonacci()}")
+if st.button("Generar secuencia de fibonacci"):
+    st.success(f"la secuencia de Fibonacci con {numero} terminos es: {algoritmos.fibonacci()}")
