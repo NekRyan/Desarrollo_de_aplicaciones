7b387c7e6b9016770608c84e3c3cdc7b8dd3bb4b
NekRyan <nekryanjm@gmail.com>
2025-02-20 21:51:44 -0500

    creando archivo de algoritmos 2.1

--- Ses1.py
+++ Ses1.py
@@ -1,14 +1,11 @@
 import streamlit as st
-from algoritmos import AlgoritmosSecuenciales
 
-st.title("Algoritmos secuenciales con POO Streamlit")
+#Titulo de la aplicacion
+st.title("Bievenido a mi aplicación")
 
-#entrada de usuario
-numero = st.number_input("ingrese un numero entero positivo", min_value=1, value=5, step=1)
+#Solicitar nombre de usuario
+nombre = st.text_input("Por favor ingrese su nombre")
 
-#Instanciamos la clase con el numero ingresado
-algoritmos = algoritmosSecunciales(numero)
-
-#Crear botones para ejecutar los algoritmos secunciales
-if st.button("calcular suma de N numeros"):
-    st.success(f"la suma de los primeros {numero} numero es:{algoritmos.suma_n_numeros()}")
+#Mostrar saludo si el usuario ingresa su nombre
+if nombre:
+    st.write(f"Hola, {nombre}! Bievenido a esta aplicación web de streamlit")
