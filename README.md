7d28cc471bcbdeff72a4ef0674d1a76f3855468f
NekRyan <nekryanjm@gmail.com>
2025-02-25 21:40:49 -0500

    creando documento del proyecto 3 - definiendo proyecto

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 url de proyecto 1: 
 url de proyecto 2: https://desarrollodeaplicaciones-a5xefuhpm7xbxodk8obnxa.streamlit.app/
-url de proyecto 3: https://desarrollodeaplicaciones-kst8js23yvtq54yarez33t.streamlit.app/
+url de proyecto 3: https://desarrollodeaplicaciones-vawxj9amyccfxdstlrhysq.streamlit.app/
--- Ses3.py
+++ Ses3.py
@@ -1,4 +1,23 @@
 import streamlit as st
 
-st.title("Clasificación de Puntajes")
+#Funcion para clasificar puntaje
+def clasificar_puntaje(puntaje):
+    if puntaje >= 85:
+        return "Excelente"
+    elif puntaje >= 70:
+        return "Bueno"
+    else:
+        return "Necesita Mejorar"
+
+#Interfaz en Streamlit
+st.title("Clasificacion de Puntajes") 
+st.title("Ingrese un puntaje y el sistema lo clasificará") 
+
+#Entrada de usuario
+puntaje = st.number_input("Ingrese un puntaje (0-100):", min_value=0, max_value=100, step=1)
+
+#Boton para clasificar
+if st.button("Clasificar"):
+    resultado = clasificar_puntaje(puntaje)
+    st.success(f"El puntaje {puntaje} es clasficado como: {resultado}")
 
