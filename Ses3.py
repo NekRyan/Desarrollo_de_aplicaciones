79a0e75b293813539294b5858421cfa076f5b91d
NekRyan <nekryanjm@gmail.com>
2025-02-25 22:17:24 -0500

    agregando menu desplegable

--- Ses3.py
+++ Ses3.py
@@ -9,15 +9,32 @@ def clasificar_puntaje(puntaje):
     else:
         return "Necesita Mejorar"
 
-#Interfaz en Streamlit
-st.title("Clasificacion de Puntajes") 
-st.title("Ingrese un puntaje y el sistema lo clasificará") 
+#Menú Lateral
+st.sidebar.title("Menú de navegación")
+opcion = st.sidebar._selectbox("Seleccione una opción", ["Inicio", "Clasificación de Puntajes"])
 
-#Entrada de usuario
-puntaje = st.number_input("Ingrese un puntaje (0-100):", min_value=0, max_value=100, step=1)
+#Sección: Inicio
+if opcion == "Inicio":
+    st.title("Bienvenido a la Aplicación")
+    st.write("Mueva el deslizador para como se clasifica el puntaje en tiempo real")
+    
+    #Filtro desplazable
+    puntaje_slider = st.slider("Seleccione un puntaje:", 0, 100, 50)
 
-#Boton para clasificar
-if st.button("Clasificar"):
-    resultado = clasificar_puntaje(puntaje)
-    st.success(f"El puntaje {puntaje} es clasficado como: {resultado}")
+    #Mostrar la clasificacion en tiempo real
+    st.info(f"El puntaje {puntaje_slider} es clasificado como: **{clasificar_puntaje(puntaje_slider)}**")
+
+
+#Sección: Clasificacion de Puntajes
+elif opcion == "Clasificacion de Puntajes":
+    st.title("Clasificacion de Puntajes") 
+    st.write("Ingrese un puntaje y el sistema lo clasificará.") 
+
+    #Entrada de usuario
+    puntaje = st.number_input("Ingrese un puntaje (0-100):", min_value=0, max_value=100, step=1)
+
+    #Boton para clasificar
+    if st.button("Clasificar"):
+        resultado = clasificar_puntaje(puntaje)
+        st.success(f"El puntaje {puntaje} es clasficado como: {resultado}")
 
