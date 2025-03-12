e3287b64d454f94894411791e54b064bdfeec021
NekRyan <nekryanjm@gmail.com>
2025-03-11 22:12:08 -0500

    Trabajando con Supabase y Streamlit-1

--- Ses5.py
+++ Ses5.py
@@ -6,7 +6,7 @@ import os
 
 SUPABASE_URL = "https://bmuksajpluhszplulsvv.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtdWtzYWpwbHVoc3pwbHVsc3Z2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3NDU1MDIsImV4cCI6MjA1NzMyMTUwMn0.FxisTKSWk17XsI3E9oypN9MFRfVHSDuERcXz2zDcECg"
-supabase: Client = create_client(supabase_url, supabase_key)
+supabase: client = create_client(SUPABASE_KEY, SUPABASE_URL)
 
 st.title("Gestion de clientes - CRUD con Supabase y Streamlit")
 
