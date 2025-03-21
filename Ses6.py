0217006a6891f5bc5cabc95c243de1441a80296e
NekRyan <nekryanjm@gmail.com>
2025-03-21 13:05:35 -0500

    Trabajando en nuevo supabase ses6

---
+++ Ses6.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import os
+from supabase import create_client, Client
+
+#configurar supabase
+
+SUPABASE_URL = "https://bmuksajpluhszplulsvv.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtdWtzYWpwbHVoc3pwbHVsc3Z2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3NDU1MDIsImV4cCI6MjA1NzMyMTUwMn0.FxisTKSWk17XsI3E9oypN9MFRfVHSDuERcXz2zDcECg"
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+st.title("Gesti[on de Clientes CRUD Supabase y Streamlit]")
+
+#Formulario agregar cliente
+
+st.header("Agregar Cliente")
+nombre = st.text_input("Nombre")
+email = st.text_input("Email")
+telefono = st.text_input("Telefono")
+
+if st.button("Agregar Cliente"):
+    if nombre and email:
+        data = {"nombre": nombre, "email": email, "telefono": telefono}
+        response = supabase.table("Clientes").insert(data).execute()
+        st.success("Clientes agregados correctamente")
+    else: 
+        st.warning("Nombre y Email son obligatorios")
